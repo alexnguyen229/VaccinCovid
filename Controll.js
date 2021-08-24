c883950b42e876ab5c78f51e2e486930d71f771c
alexnguyen229 <82994964+alexnguyen229@users.noreply.github.com>
2021-08-25 00:10:25 +0700

    Add files via upload

---
+++ Controll.js
@@ -0,0 +1,12 @@
+const listOfPeople = [
+    { name:'A', age:21, sex:'Male'   },
+    { name:'B', age:10, sex:'Female' },
+    { name:'C', age:37, sex:'Male'   },
+    { name:'D', age:25, sex:'Male'   },
+    { name:'E', age:41, sex:'Female' },
+    { name:'F', age:66, sex:'Male'   },
+    { name:'G', age:81, sex:'Female' }
+
+];
+
+export default listOfPeople;
